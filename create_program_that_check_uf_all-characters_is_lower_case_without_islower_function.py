81b42979988793ff288b2e65f7ccf720459c5f22
Batsatsugo <corajianchristian@gmail.com>
2025-03-30 12:57:02 +0800

    program finished

--- create_program_that_check_uf_all-characters_is_lower_case_without_islower_function.py
+++ create_program_that_check_uf_all-characters_is_lower_case_without_islower_function.py
@@ -1,11 +1,19 @@
 # DECLARE word, all_lowercase
+
 # word = input("Enter a word: ")
+word = input("Enter a word: ")
+
 # all_lowercase = True
 # FOR char in word:
 #     IF 'A' <= char <= 'Z':
 #         all_lowercase = False
 #         BREAK
-# IF all_lowercase:
-#     PRINT("The word is completely in lowercase.")
-# ELSE:
-#     PRINT("The word is not completely in lowercase.")
+all_lowercase = True
+for char in word:
+     if 'A' <= char <= 'Z':
+         all_lowercase = False
+         break
+if all_lowercase:
+    print("The word is completely in lowercase.")
+else:
+    print("The word is not completely in lowercase.")
