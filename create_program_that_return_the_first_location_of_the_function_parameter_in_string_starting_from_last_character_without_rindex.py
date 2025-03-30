fdd58b4f894606df1ba51817b546c8df94cd0c0c
Batsatsugo <corajianchristian@gmail.com>
2025-03-30 14:16:54 +0800

    pseudocode added

---
+++ create_program_that_return_the_first_location_of_the_function_parameter_in_string_starting_from_last_character_without_rindex.py
@@ -0,0 +1,11 @@
+# DECLARE word, char, found
+# word = input("Enter the word: ")
+# char = input("Enter the character to find: ")
+# found = False
+# FOR i in range(len(word) - 1, -1, -1):
+#     IF word[i] == char:
+#         PRINT(f"The character '{char}' last appears at index {i}.")
+#         found = True
+#         BREAK
+# IF not found:
+#     PRINT(f"The character '{char}' is not found in the word.")
