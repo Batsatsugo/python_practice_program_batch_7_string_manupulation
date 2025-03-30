5e246573e9e4c713708c6b69d7c92962cbec42c7
Batsatsugo <corajianchristian@gmail.com>
2025-03-30 11:19:45 +0800

    pseudocode added

---
+++ create_program_that_removes_characters_at_end_of_the_string_that_matches_function_parameter_without_removesuffix_function.py
@@ -0,0 +1,6 @@
+# DECLARE word, suffix
+# word = input("Enter a word: ")
+# suffix = input("Enter the suffix to remove: ")
+# IF word.endswith(suffix):
+#     word = word[:len(word) - len(suffix)]
+# PRINT("Modified word:", word)
