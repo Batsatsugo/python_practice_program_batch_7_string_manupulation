8949b98b9884af40d4067b8c139522fc78088602
Batsatsugo <corajianchristian@gmail.com>
2025-03-30 12:49:01 +0800

    finished code

--- create_program_that_converts_all_characters_to_upper_case_without_using_upper_function.py
+++ create_program_that_converts_all_characters_to_upper_case_without_using_upper_function.py
@@ -1,9 +1,19 @@
 # DECLARE word, uppercase_word
+
 # word = input("Enter a word: ")
+word = input("Enter a word: ")
+
 # uppercase_word = ""
 # for char in word:
 #     if 'a' <= char <= 'z':
 #         uppercase_word += chr(ord(char) - 32)
 #     else:
 #         uppercase_word += char
+uppercase_word = ""
+for char in word:
+    if 'a' <= char <= 'z':
+        uppercase_word += chr(ord(char) - 32)
+    else:
+         uppercase_word += char
 # print("Uppercase word:", uppercase_word)
+print("Uppercase word:", uppercase_word)
