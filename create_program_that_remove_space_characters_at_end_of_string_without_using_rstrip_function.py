7ce44b3be4d4e034caf336135a6a72a858777912
Batsatsugo <corajianchristian@gmail.com>
2025-03-30 11:13:11 +0800

    program finished

--- create_program_that_remove_space_characters_at_end_of_string_without_using_rstrip_function.py
+++ create_program_that_remove_space_characters_at_end_of_string_without_using_rstrip_function.py
@@ -1,7 +1,16 @@
 # DECLARE word, index, trimmed_word
-# word = input("Enter a string: ")
+
+# word = input("Enter a word: ")
+word = input("Enter a word: ")
+
 # index = len(word) - 1
 # WHILE i >= 0 and word[i] == " ":
 #     i -= 1
+index = len(word) - 1
+while index >= 0 and word[index] == " ":
+    index -= 1
+
 # trimmed_word = word[:i + 1]
 # PRINT("Trimmed word:", trimmed_word)
+trimmed_word = word[:index + 1]
+print("Trimmed word:", trimmed_word)
