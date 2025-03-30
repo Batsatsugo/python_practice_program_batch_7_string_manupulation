45a8ae9a571f53d262cda646a7c2c688d3ed2aec
Batsatsugo <corajianchristian@gmail.com>
2025-03-30 11:08:13 +0800

    pseudocode added

---
+++ create_program_that_remove_space_characters_at_end_of_string_without_using_rstrip_function.py
@@ -0,0 +1,7 @@
+# DECLARE word, index, trimmed_word
+# word = input("Enter a string: ")
+# index = len(word) - 1
+# WHILE i >= 0 and word[i] == " ":
+#     i -= 1
+# trimmed_word = word[:i + 1]
+# PRINT("Trimmed word:", trimmed_word)
