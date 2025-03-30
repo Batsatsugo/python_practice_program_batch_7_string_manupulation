38990adcbefcfd212127bb081778be9aa5678899
Batsatsugo <corajianchristian@gmail.com>
2025-03-30 12:54:51 +0800

    file craeted and pseudocode added

---
+++ create_program_that_check_uf_all-characters_is_lower_case_without_islower_function.py
@@ -0,0 +1,11 @@
+# DECLARE word, all_lowercase
+# word = input("Enter a word: ")
+# all_lowercase = True
+# FOR char in word:
+#     IF 'A' <= char <= 'Z':
+#         all_lowercase = False
+#         BREAK
+# IF all_lowercase:
+#     PRINT("The word is completely in lowercase.")
+# ELSE:
+#     PRINT("The word is not completely in lowercase.")
