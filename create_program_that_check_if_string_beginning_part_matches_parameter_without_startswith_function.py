6ac1ebc3969c690d52cf80c65cf66819fca95271
Batsatsugo <corajianchristian@gmail.com>
2025-03-30 13:04:09 +0800

    pseudocode added

---
+++ create_program_that_check_if_string_beginning_part_matches_parameter_without_startswith_function.py
@@ -0,0 +1,15 @@
+# DECLARE word, prefix, is_starting
+# word = input("Enter the word: ")
+# prefix = input("Enter the prefix to check: ")
+# is_starting = True
+# IF len(prefix) > len(word):
+#     is_starting = False
+# ELSE:
+#     FOR i in range(len(prefix)):
+#         IF word[i] != prefix[i]:
+#             is_starting = False
+#             BREAK
+# IF is_starting:
+#     PRINT("The word starts with the given prefix.")
+# ELSE:
+#     PRINT("The word does NOT start with the given prefix.")
