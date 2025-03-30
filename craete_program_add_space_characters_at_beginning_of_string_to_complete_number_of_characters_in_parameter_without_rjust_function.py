adc79b0663ffc45b26111b28d654d33e18d9413e
Batsatsugo <corajianchristian@gmail.com>
2025-03-30 13:15:13 +0800

    finished code

--- craete_program_add_space_characters_at_beginning_of_string_to_complete_number_of_characters_in_parameter_without_rjust_function.py
+++ craete_program_add_space_characters_at_beginning_of_string_to_complete_number_of_characters_in_parameter_without_rjust_function.py
@@ -1,7 +1,16 @@
 # DECLARE word, width, space_needed
+
 # word = input("Enter the word: ")
 # width = int(input("Enter the total width: "))
+word = input("Enter the word: ")
+width = int(input("Enter the total width: "))
+
 # spaces_needed = width - len(word)
 # IF spaces_needed > 0:
 #     word = " " * spaces_needed + word
+spaces_needed = width - len(word)
+if spaces_needed > 0:
+     word = " " * spaces_needed + word
+
 # PRINT("Right-justified word:", word)
+print("Right-justified word:", word)
