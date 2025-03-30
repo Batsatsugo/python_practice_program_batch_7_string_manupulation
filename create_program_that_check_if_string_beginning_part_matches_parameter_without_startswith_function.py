a64eb7022a1c31f8c87b50016b71edcfda4abfaf
Batsatsugo <corajianchristian@gmail.com>
2025-03-30 13:06:31 +0800

    finished code

--- create_program_that_check_if_string_beginning_part_matches_parameter_without_startswith_function.py
+++ create_program_that_check_if_string_beginning_part_matches_parameter_without_startswith_function.py
@@ -1,6 +1,10 @@
 # DECLARE word, prefix, is_starting
+
 # word = input("Enter the word: ")
 # prefix = input("Enter the prefix to check: ")
+word = input("Enter the word: ")
+prefix = input("Enter the prefix to check: ")
+
 # is_starting = True
 # IF len(prefix) > len(word):
 #     is_starting = False
@@ -9,7 +13,20 @@
 #         IF word[i] != prefix[i]:
 #             is_starting = False
 #             BREAK
+is_starting = True
+if len(prefix) > len(word):
+     is_starting = False
+else:
+     for i in range(len(prefix)):
+         if word[i] != prefix[i]:
+             is_starting = False
+             break
+
 # IF is_starting:
 #     PRINT("The word starts with the given prefix.")
 # ELSE:
 #     PRINT("The word does NOT start with the given prefix.")
+if is_starting:
+     print("The word starts with the given prefix.")
+else:
+     print("The word does NOT start with the given prefix.")
