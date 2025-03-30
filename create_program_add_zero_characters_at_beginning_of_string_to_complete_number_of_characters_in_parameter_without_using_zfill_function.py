142f132eced7389bece514ed9a3113beb41e431e
Batsatsugo <corajianchristian@gmail.com>
2025-03-30 13:21:52 +0800

    pseudocode added

---
+++ create_program_add_zero_characters_at_beginning_of_string_to_complete_number_of_characters_in_parameter_without_using_zfill_function.py
@@ -0,0 +1,7 @@
+# DECLARE text, width, zeros_needed
+# text = input("Enter the string: ")
+# width = int(input("Enter the total width: "))
+# zeros_needed = width - len(text)
+# IF zeros_needed > 0:
+#     text = "0" * zeros_needed + text
+# PRINT("Zero-filled string:", text)
